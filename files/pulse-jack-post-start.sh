34160799bdc9c588d436bd0a9e01f5e43f742540
Ernesto Crespo <ecrespo@gmail.com>
2012-08-07 20:38:29 -0430

    Eliminados archivos de repo github

--- files/pulse-jack-post-start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-## Carga los modulos para que el Servidor de Sonido Jack y Pulse Audio convivan luego del inicio del Servidor de Sonido Jack
-## By: José Leonel Subero Gamarra
-## Licencias: GPLv3
-## Fuentes de consulta: http://rivendell.tryphon.org/wiki/Debian_6_Setup_JACK2_with_Pulse_Audio ; http://jackaudio.org/
-pactl load-module module-jack-sink channels=2
-pactl load-module module-jack-source channels=2
-pacmd set-default-sink jack_out
-pacmd set-default-source jack_in
--- files/pulse-jack-pre-start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-## Suspende el Servidor de SonidovPulse Audio para liberar la tarjeta de sonido para que el Servidor de Sonido Jack sea el servidor de sonidos principal del sistema
-## By: José Leonel Subero Gamarra
-## Licencias: GPLv3
-## Fuentes de consulta: http://rivendell.tryphon.org/wiki/Debian_6_Setup_JACK2_with_Pulse_Audio ; http://jackaudio.org/
-pacmd suspend true
